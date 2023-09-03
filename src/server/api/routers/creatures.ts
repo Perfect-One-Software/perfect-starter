9dedfa5d4cdeafc7482f860f0e4e43d09c3d0d5e
NorweskiDrwal <devutnia@gmail.com>
2023-09-03 22:52:59 +0200

    added form for adding creatures to location

--- src/server/api/routers/creatures.ts
+++ src/server/api/routers/creatures.ts
@@ -6,23 +6,28 @@ export const creaturesRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
     return await ctx.prisma.creature.findMany();
   }),
+
+  getByLocationId: publicProcedure
+    .input(z.object({ locationId: CreatureSchema.shape.locationId }))
+    .query(async ({ ctx, input }) => {
+      return await ctx.prisma.creature.findMany({ where: { locationId: input.locationId } });
+    }),
+
   edit: publicProcedure
     .input(
       z.object({
         id: CreatureSchema.shape.id,
+        image: CreatureSchema.shape.image.optional(),
         species: CreatureSchema.shape.species.optional(),
         colorName: CreatureSchema.shape.colorName.optional(),
-        image: CreatureSchema.shape.image.optional(),
         locationId: CreatureSchema.shape.locationId.optional(),
       }),
     )
     .mutation(async ({ ctx, input }) => {
       const { id, ...data } = input;
-      return await ctx.prisma.creature.update({
-        where: { id },
-        data,
-      });
+      return await ctx.prisma.creature.update({ where: { id }, data });
     }),
+
   create: publicProcedure
     .input(
       CreatureSchema.pick({
@@ -33,21 +38,12 @@ export const creaturesRouter = createTRPCRouter({
       }),
     )
     .mutation(async ({ ctx, input }) => {
-      return await ctx.prisma.creature.create({
-        data: input,
-      });
+      return await ctx.prisma.creature.create({ data: input });
     }),
+
   delete: publicProcedure
-    .input(
-      z.object({
-        id: CreatureSchema.shape.id,
-      }),
-    )
-    .mutation(async ({ ctx, input }) => {
-      return await ctx.prisma.creature.delete({
-        where: {
-          id: input.id,
-        },
-      });
+    .input(z.object({ id: CreatureSchema.shape.id }))
+    .mutation(async ({ ctx, input: { id } }) => {
+      return await ctx.prisma.creature.delete({ where: { id } });
     }),
 });
--- src/server/api/routers/locations.ts
+++ src/server/api/routers/locations.ts
@@ -1,7 +1,16 @@
+import { z } from "zod";
+
 import { createTRPCRouter, publicProcedure } from "../trpc";
+import { LocationSchema } from "~/server/generated";
 
 export const locationsRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
     return await ctx.prisma.location.findMany();
   }),
+
+  getById: publicProcedure
+    .input(z.object({ locationId: LocationSchema.shape.id }))
+    .query(async ({ ctx, input: { locationId } }) => {
+      return await ctx.prisma.location.findUnique({ where: { id: locationId } });
+    }),
 });
