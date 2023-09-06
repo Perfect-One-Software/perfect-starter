7b4de7d8387f2ba999cc339033791cb22474d12f
Michał <mm.fabijanski@gmail.com>
2023-09-06 18:01:01 +0200

    Initialize React Hook Form, register the form fields

--- src/pages/creatures/add.tsx
+++ src/pages/creatures/add.tsx
@@ -1,6 +1,32 @@
 import Head from "next/head";
+import { useForm, type SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+  image: string;
+  species: string;
+  colorName: string;
+  locationId: string;
+};
 
 const AddCreature = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (formData) => {
+    console.log({ formData });
+  };
+
+  console.log({
+    image: watch("image"),
+    species: watch("species"),
+    colorName: watch("colorName"),
+    locationId: watch("locationId"),
+  });
+
   return (
     <>
       <Head>
@@ -10,20 +36,27 @@ const AddCreature = () => {
         <h1 className="mb-3 text-xl font-medium text-white">
           Wprowadź dane zwierzęia
         </h1>
-        <form className="flex w-[400px] flex-col gap-y-2">
+        <form
+          onSubmit={(event) => void handleSubmit(onSubmit)(event)}
+          className="flex w-[400px] flex-col gap-y-2"
+        >
           <label className="flex flex-col">
             <span className="text-sm font-medium text-white">Gatunek</span>
-            <input type="text" className="input" />
+            <input {...register("species")} type="text" className="input" />
           </label>
           <label className="flex flex-col">
             <span className="text-sm font-medium text-white">Kolor</span>
-            <input type="text" className="input" />
+            <input {...register("colorName")} type="text" className="input" />
           </label>
           <label className="flex flex-col">
             <span className="text-sm font-medium text-white">
               Lokalizacja ośrodka hodowlanego
             </span>
-            <select className="select" defaultValue={1}>
+            <select
+              {...register("locationId")}
+              className="select"
+              defaultValue={1}
+            >
               <option value={1}>ośrodek 1</option>
               <option value={2}>ośrodek 2</option>
             </select>
@@ -31,6 +64,7 @@ const AddCreature = () => {
           <label className="flex flex-col">
             <span className="text-sm font-medium text-white">Kolor</span>
             <input
+              {...register("image")}
               type="file"
               accept="image/*"
               className="file-input"
