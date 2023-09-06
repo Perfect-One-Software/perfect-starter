47d4c07f14517ce35922e50ae7edb483dc08bbc1
Michał <mm.fabijanski@gmail.com>
2023-09-06 18:53:31 +0200

    Add zodResolver, validation messages and required fields markers

--- src/pages/creatures/add.tsx
+++ src/pages/creatures/add.tsx
@@ -1,8 +1,22 @@
+import { zodResolver } from "@hookform/resolvers/zod";
 import Head from "next/head";
 import { useForm, type SubmitHandler } from "react-hook-form";
-import { type RouterInputs, api } from "~/utils/api";
+import { z } from "zod";
+import { CreatureSchema } from "~/server/generated";
+import { api } from "~/utils/api";
 
-type SchemaType = RouterInputs["creatures"]["create"];
+const CreateInputSchema = CreatureSchema.pick({
+  image: true,
+  species: true,
+  colorName: true,
+  locationId: true,
+}).extend({
+  locationId: z.string().refine((value) => value !== "", "Uzupełnij pole"),
+  species: z.string().refine((value) => value !== "", "Uzupełnij pole"),
+  colorName: z.string().refine((value) => value !== "", "Uzupełnij pole"),
+});
+
+type CreateInputSchemaType = z.infer<typeof CreateInputSchema>;
 
 const AddCreature = () => {
   const {
@@ -10,7 +24,9 @@ const AddCreature = () => {
     handleSubmit,
     watch,
     formState: { errors },
-  } = useForm<SchemaType>();
+  } = useForm<CreateInputSchemaType>({
+    resolver: zodResolver(CreateInputSchema),
+  });
   console.log("🚀 ~ file: add.tsx:13 ~ AddCreature ~ errors:", errors);
 
   const { data: locations = [], isLoading: isLoadingLocations } =
@@ -18,7 +34,7 @@ const AddCreature = () => {
 
   const { mutate } = api.creatures.create.useMutation();
 
-  const onSubmit: SubmitHandler<SchemaType> = (data) => {
+  const onSubmit: SubmitHandler<CreateInputSchemaType> = (data) => {
     console.log("formData: ", data);
 
     // FIXME: handle image file to string (base64) conversion; 'image: null' value just for development purposes
@@ -41,49 +57,84 @@ const AddCreature = () => {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <h1 className="mb-3 text-xl font-medium text-white">
-          Wprowadź dane zwierzęia
+          Wprowadź dane zwierzęcia
         </h1>
         <form
           onSubmit={(event) => void handleSubmit(onSubmit)(event)}
           className="flex w-[400px] flex-col gap-y-2"
         >
-          <label className="flex flex-col">
-            <span className="text-sm font-medium text-white">Gatunek</span>
-            <input {...register("species")} type="text" className="input" />
-          </label>
-          <label className="flex flex-col">
-            <span className="text-sm font-medium text-white">Kolor</span>
-            <input {...register("colorName")} type="text" className="input" />
-          </label>
-          <label className="flex flex-col">
-            <span className="text-sm font-medium text-white">
-              Lokalizacja ośrodka hodowlanego
-            </span>
-            <select
-              {...register("locationId")}
-              className="select"
-              defaultValue=""
-            >
-              <option value="" disabled>
-                Wybierz ośrodek hodowlany
-              </option>
-              {locations.map(({ id, name }) => (
-                <option key={id} value={id}>
-                  {name}
+          <fieldset>
+            <label className="flex flex-col">
+              <span className="text-sm font-medium text-white after:ml-0.5 after:text-red-500 after:content-['*']">
+                Gatunek
+              </span>
+              <input {...register("species")} type="text" className="input" />
+            </label>
+            {errors.species && (
+              <span className="text-sm text-red-500">
+                {errors.species.message}
+              </span>
+            )}
+          </fieldset>
+          <fieldset>
+            <label className="flex flex-col">
+              <span className="text-sm font-medium text-white after:ml-0.5 after:text-red-500 after:content-['*']">
+                Kolor
+              </span>
+              <input {...register("colorName")} type="text" className="input" />
+            </label>
+            {errors.colorName && (
+              <span className="text-sm text-red-500">
+                {errors.colorName.message}
+              </span>
+            )}
+          </fieldset>
+          <fieldset>
+            <label className="flex flex-col">
+              <span className="text-sm font-medium text-white after:ml-0.5 after:text-red-500 after:content-['*']">
+                Lokalizacja ośrodka hodowlanego
+              </span>
+              <select
+                {...register("locationId")}
+                className="select"
+                defaultValue=""
+              >
+                <option value="" disabled>
+                  Wybierz ośrodek hodowlany
                 </option>
-              ))}
-            </select>
-          </label>
-          <label className="flex flex-col">
-            <span className="text-sm font-medium text-white">Kolor</span>
-            <input
-              {...register("image")}
-              type="file"
-              accept="image/*"
-              className="file-input"
-              multiple={false}
-            />
-          </label>
+                {locations.map(({ id, name }) => (
+                  <option key={id} value={id}>
+                    {name}
+                  </option>
+                ))}
+              </select>
+            </label>
+            {errors.locationId && (
+              <span className="text-sm text-red-500">
+                {errors.locationId.message}
+              </span>
+            )}
+          </fieldset>
+          <fieldset>
+            <label className="flex flex-col">
+              <span className="text-sm font-medium text-white">Kolor</span>
+              <input
+                {...register("image")}
+                // FIXME: type changed to "text" to handle the image upload at the later stage
+                type="text"
+                // type="file"
+                // accept="image/*"
+                // className="file-input"
+                // multiple={false}
+              />
+            </label>
+            {errors.image && (
+              <span className="text-sm text-red-500">
+                {errors.image.message}
+              </span>
+            )}
+          </fieldset>
+
           <button
             type="submit"
             className="btn btn-primary mt-3 w-[100px] self-center"
